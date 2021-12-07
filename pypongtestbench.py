6d119a54c49836ed5461579a39a05ea171252122
RichardSBS <richard.dietzsch@gmx.de>
2021-12-07 08:05:46 +0100

    hello hello

--- pypongtestbench.py
+++ pypongtestbench.py
@@ -1,32 +1,55 @@
 # import the pygame module, so you can use it
 import pygame
-green=(0,255,0)
-white=(255,255,255)
-balck=(0,0,0)
-color=green
+#Farben
+WHITE = (255,255,255)
+BLACK = (0,0,0)
+GRAY = (50,50,50)
+GREEN = (0,255,0)
 setterx=1
 settery=0
+
+posx = 200
+posy = 200
 clock = pygame.time.Clock()
-# define a main function
 
-# initialize the pygame module
+# initialisiren des pygame modules
 pygame.init()
-# load and set the logo
-#logo = pygame.image.load("python.png")
-#pygame.display.set_icon(logo)
 
-pygame.display.set_caption("minimal program")
+#So soll mein Fenster heißen
+pygame.display.set_caption("PyPong")
 
-    # create a surface on screen that has the size of 240 x 180
+#Festlegen einer variable die meine GUI beinhaltet mit den ausmaßen die ich angegeben hab
 screen = pygame.display.set_mode((800, 600))
 
+def ballRichtung(setterx,settery,x,y):
+    if setterx == 1:
+        posx = x + 4
 
-    # define a variable to control the main loop
+    if setterx == 0:
+        posx = x - 4
+
+    if settery == 1:
+        posy = y + 4
+
+    if settery == 0:
+        posy = y - 4
+    return posx,posy
+
+
+def setter(posx,posy):
+    if posx >= 740:
+        setterx = 0
+    if posx<=50:
+        setterx=1
+    if posy >= 500:
+        settery = 0
+    if posy <= 50:
+        settery = 1
+# define a variable to control the main loop
 running = True
 
     # main loop
-posx = 200
-posy = 200
+
 while running:
 
 
@@ -37,38 +60,15 @@ while running:
                 # change the value to False, to exit the main loop
             running = False
 
-    pygame.draw.rect(screen, (255, 255, 255), [10, 20, 20, 200])
-    pygame.draw.rect(screen, (255, 255, 255), [760, 10, 20, 200])
-    pygame.draw.rect(screen, color, [posx, posy, 20, 20])
+    pygame.draw.rect(screen, WHITE, [10, 20, 20, 200])
+    pygame.draw.rect(screen, WHITE, [760, 10, 20, 200])
+    pygame.draw.rect(screen, WHITE, [posx, posy, 20, 20])
 
-    #posy = posy+4
-    if setterx == 1:
-        posx = posx + 4
 
-    if setterx==0:
-        posx= posx - 4
-
-    if settery == 1:
-        posy = posy + 4
-
-    if settery == 0:
-        posy = posy - 4
-
-    if posx >= 740:
-        setterx = 0
-        color = (0, 255, 0)
-    if posx<=50:
-        setterx=1
-        color = (255, 0, 0)
-    if posy >= 500:
-        settery = 0
-        color = (255, 255, 255)
-    if posy <= 50:
-        settery = 1
-        color = (0, 0, 255)
-
+    posx,posy=ballRichtung(setterx,settery,posx,posy)
+    setter(posx,posy)
     pygame.display.flip()
-    screen.fill(balck)
+    screen.fill(BLACK)
     clock.tick(1000)
 # run the main function only if this module is executed as the main script
 # (if you import this as a module then nothing is executed)
